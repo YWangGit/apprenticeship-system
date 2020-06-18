d10f85094d6d812ac125523640d19033cd9466de
杨旺 <yangwang@yunjiglobal.com>
2020-06-18 09:36:41 +0800

    docs&配置

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,9 +1,11 @@
 module.exports = {
     title: '学徒制',
     description: "学徒制官网",
+    base: "/sziit/",
+    dest: "./docs",
     themeConfig: {
         carousels: ["/sziit.jpg", "/sziit.jpg", "/sziit.jpg"],
-        logo:'/logo.png',
-        footerText:'深圳信息职业技术学院版权所有粤ICP备11062843号'
-    }
+        logo: '/logo.png',
+        footerText: '深圳信息职业技术学院版权所有粤ICP备11062843号'
+    },
 }
